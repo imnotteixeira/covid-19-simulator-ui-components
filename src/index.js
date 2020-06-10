9f438a7a8a3d83810f7a5fbc7fbdd8e1f427fdb8
Angelo Teixeira <imnotteixeira@gmail.com>
2020-06-10 18:02:30 +0100

    Now exporting MUI Theme object

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import GeneralChart from "./GeneralChart";
 import MatrixHeatMap from "./MatrixHeatMap";
 import VisualizationsLayout from "./VisualizationsLayout";
+import AppTheme from "./AppTheme";
 
 export {
     GeneralChart,
     MatrixHeatMap,
     VisualizationsLayout,
+    AppTheme,
 };
